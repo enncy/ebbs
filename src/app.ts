d38733ba3e4be1e1b33cfe43583dd8b28fb31448
enncy <877526278@qq.com>
2024-09-12 14:39:32 +0800

    优化资源加载路径，优化错误捕获，优化文件上传参数

--- src/app.ts
+++ src/app.ts
@@ -4,39 +4,53 @@ import session from 'express-session';
 import path, { resolve } from 'path';
 import { mongo } from './mongo';
 import ConnectMongo from 'connect-mongo';
-import config from '../ebbs.config.json'
 import PluginRender from './middleware/plugin-render';
 import { loadPlugins } from './core/plugins';
 import dotenv from 'dotenv';
 import winston from 'winston';
 import chalk from 'chalk';
 import { errorPages } from './middleware/error-pages';
+import fileupload from 'express-fileupload';
+import { global_config } from 'ebbs.config';
 
 dotenv.config();
 
 ; (async () => {
     const app = express();
     // 连接数据库
-    await mongo(config.db.mongo_url.trim());
+    await mongo(global_config.db.mongo_url.trim());
     app
-        // 静态资源
-        .use("/public", express.static(path.resolve('public')))
+        // 静态资源 
+        .use("/assets", express.static(path.resolve('assets')))
+        .use("/public/assets", express.static(path.resolve('public/assets')))
+        .use("/public/uploads", express.static(path.resolve('public/uploads')))
+        // 文件上传
+        .use(fileupload({
+            useTempFiles: true,
+            tempFileDir: global_config.post.upload.tmp_dir,
+            abortOnLimit: true,
+            defCharset: 'utf-8',
+            defParamCharset: 'utf-8',
+            limits: {
+                fileSize: global_config.post.upload_file_size_limit * 1024 * 1024,
+            }
+        }))
         // 启用 session
         .use(
             session({
                 name: 'mclds-admin-session',
-                secret: config.db.mongo_session_secret.trim(), // 用来对session id相关的cookie进行签名
-                store: ConnectMongo.create({ mongoUrl: config.db.mongo_session_url.trim() }), // 本地存储session（文本文件，也可以选择其他store，比如redis的）
+                secret: global_config.db.mongo_session_secret.trim(), // 用来对session id相关的cookie进行签名
+                store: ConnectMongo.create({ mongoUrl: global_config.db.mongo_session_url.trim() }), // 本地存储session（文本文件，也可以选择其他store，比如redis的）
                 saveUninitialized: false, // 是否自动保存未初始化的会话，建议false
                 resave: false, // 是否每次都重新保存会话，建议false
                 cookie: {
-                    maxAge: config.db.mongo_session_valid_day * 24 * 60 * 60 * 1000 // 有效期一个月，单位是毫秒
+                    maxAge: global_config.db.mongo_session_valid_day * 24 * 60 * 60 * 1000 // 有效期一个月，单位是毫秒
                 }
             })
         )
         // 解析 post 数据
-        .use(express.urlencoded({ extended: false, limit: config.post.size_limit }))
-        .use(express.json({ limit: config.post.size_limit }))
+        .use(express.urlencoded({ extended: false, limit: '2mb' }))
+        .use(express.json({ limit: '2mb' }))
 
     // 加载插件
     const { plugins, context } = await loadPlugins()
@@ -63,21 +77,22 @@ dotenv.config();
     }
 
     app
-    // 渲染插件定义的页面
-        .use(PluginRender(context.getAllPages())) 
+        // 渲染插件定义的页面
+        .use(PluginRender(context.getAllPages()))
         // 错误页面
         .use(errorPages())
 
 
     // 启动服务器
-    app.listen(config.port, () => {
-        console.log('[express] server launched as http://localhost:' + config.port);
+    app.listen(global_config.port, () => {
+        console.log('[express] server launched as http://localhost:' + global_config.port);
     });
 })();
 
 
 // 代理日志
-const logger = winston.createLogger({
+export const logger = winston.createLogger({
+    format: winston.format.combine(winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }), winston.format.simple()),
     transports: [
         new winston.transports.File({ filename: resolve('logs/console.log'), lazy: true }),
         new winston.transports.File({ filename: resolve('logs/error.log'), level: 'error', lazy: true }),
@@ -88,9 +103,13 @@ const logger = winston.createLogger({
 process.on('uncaughtException', (err) => {
     console.error(chalk.redBright(err.message));
     logger.error(err.message);
+    logger.error(err.stack);
 })
 
 process.on('unhandledRejection', (reason, promise) => {
-    console.error(chalk.redBright(reason));
-    logger.error(reason);
+    promise.catch((err) => {
+        console.error(err);
+        logger.error(err.message);
+        logger.error(err.stack);
+    })
 })
